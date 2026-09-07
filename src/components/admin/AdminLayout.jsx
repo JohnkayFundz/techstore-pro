844960770194307bdc25ce8b4a655e790e55c132
John Kalumba <179124012+JohnkayFundz@users.noreply.github.com>
2026-09-07 15:22:23 +0100

    Show global toasts in admin layout

--- src/components/admin/AdminLayout.jsx
+++ src/components/admin/AdminLayout.jsx
@@ -2,53 +2,36 @@ import { Outlet } from "react-router-dom";
 
 import Sidebar from "./Sidebar";
 import AdminHeader from "./AdminHeader";
+import Toast from "../Toast.jsx";
 
 import "./AdminLayout.css";
 
-
 function AdminLayout() {
-
   return (
-
     <div className="admin-layout">
-
-
       {/* Admin Sidebar */}
       <aside
         className="admin-sidebar"
         aria-label="Admin navigation"
       >
-
         <Sidebar />
-
       </aside>
-
-
 
       {/* Admin Main Area */}
       <div className="admin-main">
-
-
         {/* Top Header */}
         <AdminHeader />
 
-
+        {/* Global Notifications */}
+        <Toast />
 
         {/* Dashboard Pages */}
         <main className="admin-content">
-
           <Outlet />
-
         </main>
-
-
       </div>
-
-
     </div>
-
   );
 }
-
 
 export default AdminLayout;
